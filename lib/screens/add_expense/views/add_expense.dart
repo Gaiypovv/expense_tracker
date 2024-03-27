4f3b37194226b7291318cd35ce326609c86e8acd
Eldar Gaiypov <eldargaiypov@MacBook-Pro-Eldar.local>
2024-03-27 16:17:10 +0600

    Set the date picker

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
+
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  void initState() {
+    dateController.text = DateFormat('dd/MM/yyyy').format(DateTime.now());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +45,7 @@ class AddExpense extends StatelessWidget {
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7,
                 child: TextFormField(
+                  controller: expenseController,
                   textAlignVertical: TextAlignVertical.center,
                   decoration: InputDecoration(
                       filled: true,
@@ -44,9 +61,10 @@ class AddExpense extends StatelessWidget {
                 ),
               ),
               const SizedBox(
-                height: 16,
+                height: 32,
               ),
               TextFormField(
+                controller: categoryController,
                 textAlignVertical: TextAlignVertical.center,
                 decoration: InputDecoration(
                     filled: true,
@@ -65,13 +83,22 @@ class AddExpense extends StatelessWidget {
                 height: 16,
               ),
               TextFormField(
+                controller: dateController,
                 textAlignVertical: TextAlignVertical.center,
-                onTap: () {
-                  showDatePicker(
+                readOnly: true,
+                onTap: () async {
+                  DateTime? newDate = await showDatePicker(
                       context: context,
                       initialDate: DateTime.now(),
                       firstDate: DateTime.now(),
-                      lastDate: DateTime.now().add(Duration(days: 365)));
+                      lastDate: DateTime.now().add(const Duration(days: 365)));
+
+                  if (newDate != null) {
+                    setState(() {
+                      dateController.text =
+                          DateFormat('dd/MM/yyyy').format(newDate);
+                    });
+                  }
                 },
                 decoration: InputDecoration(
                     filled: true,
@@ -87,9 +114,22 @@ class AddExpense extends StatelessWidget {
                         borderSide: BorderSide.none)),
               ),
               const SizedBox(
-                height: 16,
+                height: 32,
               ),
-              TextButton(onPressed: () {}, child: const Text('Save'))
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12))),
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(fontSize: 22, color: Colors.white),
+                    )),
+              )
             ],
           ),
         ),
