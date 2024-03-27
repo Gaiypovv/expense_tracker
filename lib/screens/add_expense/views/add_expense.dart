60d36b586fcddea44c34d0c7a344ea3475280efb
Eldar Gaiypov <eldargaiypov@MacBook-Pro-Eldar.local>
2024-03-27 14:53:10 +0600

    Added the expense button implementation and created the Added expense class, added the text Add Expense

---
+++ lib/screens/add_expense/views/add_expense.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddExpense extends StatelessWidget {
+  const AddExpense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Add Expenses',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
